2325e1496babbfcbbca33f7e114f51db8dd39487
ryan10145 <chang.ryan1@yahoo.com>
2016-12-03 16:23:16 -0500

    Moved back to ints

--- src/reference/MathHelper.java
+++ src/reference/MathHelper.java
@@ -12,22 +12,14 @@ public class MathHelper
 		else return 0;
 	}
 
-	public static double sign(double number)
-	{
-		if(number > 0) return 1;
-		else if(number < 0) return -1;
-		else return 0;
-	}
-
-
-	public static boolean checkCollisionWithTileMap(double x, double y, TileMap tileMap, int playerWidth, int playerHeight)
+	public static boolean checkCollisionWithTileMap(int x, int y, TileMap tileMap, int playerWidth, int playerHeight)
 	{
 		boolean collisionFlag = false;
 
-		int leftColumn = (int) (x - playerWidth / 2) / tileMap.getWidthOfTile();
-		int rightColumn = (int) (x + playerWidth / 2) / tileMap.getWidthOfTile();
-		int topRow = (int) (y - playerHeight / 2) / tileMap.getHeightOfTile();
-		int bottomRow = (int) (y + playerHeight / 2) / tileMap.getHeightOfTile();
+		int leftColumn = (x - playerWidth / 2) / tileMap.getWidthOfTile();
+		int rightColumn = (x + playerWidth / 2) / tileMap.getWidthOfTile();
+		int topRow = (y - playerHeight / 2) / tileMap.getHeightOfTile();
+		int bottomRow = (y + playerHeight / 2) / tileMap.getHeightOfTile();
 
 		if(leftColumn < 0) leftColumn = 0;
 		if(rightColumn > tileMap.getColumns() - 1) rightColumn = tileMap.getColumns() - 1;
